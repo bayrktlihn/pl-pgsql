9bb0a685deef6b25b739f43e74ab431126c59213
bayrktlihn <36965493+bayrktlihn@users.noreply.github.com>
2026-02-12 23:03:10 +0300

    Update aylık_maskeleme.sql

--- aylık_maskeleme.sql
+++ aylık_maskeleme.sql
@@ -11,6 +11,7 @@ declare
 	v_included_end date;
 	v_result bigint;
 	v_offset int := 0;
+	v_included_day_count int := 0;
 begin
 	if p_start > p_end_included then
 		raise exception 'p_start must not be greater than p_end_included';
@@ -31,18 +32,15 @@ begin
 		return 0;
 	end if;
 
-	v_result := (cast(1 as bigint) << (v_included_end - v_included_start + 1)) - 1 ;
+	v_included_day_count := v_included_end - v_included_start + 1;
+
+	v_result := (cast(1 as bigint) << v_included_day_count) - 1 ;
 
 	v_offset := cast(v_included_start - p_period_start as int);
 
 
 	return cast((v_result << v_offset) as int );
+	
+
 end;
 $$
-
-select fn_monthly_mask(
-	cast('2026-01-13' as date),
-	cast('2026-01-13' as date),
-	cast('2026-01-15' as date),
-	cast('2026-02-14' as date)
-);
